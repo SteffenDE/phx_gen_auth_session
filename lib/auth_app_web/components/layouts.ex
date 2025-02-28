fd450263a267ea6fdacb459de081557cacaa559d
Steffen Deusch <steffen@deusch.me>
2025-02-28 16:32:55 +0100

    initial commit

---
+++ lib/auth_app_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule AuthAppWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use AuthAppWeb, :controller` and
+  `use AuthAppWeb, :live_view`.
+  """
+  use AuthAppWeb, :html
+
+  embed_templates "layouts/*"
+end
